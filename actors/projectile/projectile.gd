bad727e9231e3f799bc35176da5bfcbc3b14a306
Peter Gyory <ptgyory@gmail.com>
2024-09-18 11:27:48 -0600

    week 4 day 1

--- actors/projectile/projectile.gd
+++ actors/projectile/projectile.gd
@@ -10,6 +10,9 @@ func fire(forward: Vector2, speed: float):
 func _physics_process(delta):
     position += velocity * delta
 
-
 func _on_time_to_live_timeout():
+    queue_free()
+
+func _on_body_entered(body: Node2D) -> void:
+    (body as Enemy).hit(1)
     queue_free()
